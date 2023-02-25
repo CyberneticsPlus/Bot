4d05e055398d0013853921fad7027ed1e6ed07cb
CyberneticsPlus <126213735+CyberneticsPlus@users.noreply.github.com>
2023-02-25 19:37:26 +0530

    Create index.js

---
+++ node_modules/public/server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes);
+
+// Connect to MongoDB
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+// Start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
